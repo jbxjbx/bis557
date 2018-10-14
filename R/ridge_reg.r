4c7285e4bc6e56770fc3551ce2a58e66010dff74
jbxjbx <jbxjbxjbxjbxjbx@hotmail.com>
2018-10-14 00:28:54 -0400

    HW2 ridge_reg

---
+++ R/ridge_reg.r
@@ -0,0 +1,34 @@
+#' Fit a ridge regression model
+#'
+#' @description This function passes parameters to the ridge function.
+#' @param formula a formula
+#' @param lamda a parameter in numberic values
+#' @param data a data.frame
+#' @return An ridge regression object
+#' @importFrom stats lm
+#' @importFrom stats model.matrix terms
+#' @examples
+#' fit <- ridge_reg(Sepal.Length ~.,1.2, iris)
+#' summary(fit)
+#' @export
+
+# reference to the code taught in class by Prof. Kane
+ridge_reg <- function(formula, lambda, data) {
+  rownames(data) <- NULL
+  m <- model.matrix(formula.data)
+  y <- matrix(data[,as.character(formula)[2]],ncol=1)
+  y <- y[as.numeric(rownames(m)),,drop = FALSE]
+  
+  # then fit directly by solve the matrix via svd
+  svd_obj <- svd(m)
+  U <- svd_obj$u
+  V <- svd_obj$v
+  svals <- svd_obj$d
+  
+  D = diag(svals / (svals^2 + lambda))
+  beta = V %*% D %*% t(U) %*% y
+  rownames(beta) = colnames(m)
+  ret = list(coefficients = beta, lambda = lambda, formula=formula)
+  class(ret) = "ridge_reg"
+  ret
+}
